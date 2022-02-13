b1e8b8e332ea9ce734c4b4a278479a743c4e44f9
Zahid097 <98472087+Zahid097@users.noreply.github.com>
2022-02-13 10:27:39 +0300

    HomeWork16.

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -5,28 +5,35 @@ public class Circle {
     private static double radius;
 
     static void area() {
-        double i = PI* (radius * radius);
-        System.out.println("Площадь area: "+ i);
+        double i = PI * (radius * radius);
+        System.out.println("Площадь area: " + i);
     }
+
     static void circumference() {
-        double i = (PI * 2 *radius);
-        System.out.println("Длина radius: "+ i);
+        double i = (PI * 2 * radius);
+        System.out.println("Длина radius: " + i);
     }
+
     public Circle() {
     }
+
     public Circle(double PI, double radius) {
         this.PI = PI;
         this.radius = radius;
     }
+
     public double getPI() {
         return PI;
     }
+
     public void setPI(double PI) {
         this.PI = PI;
     }
+
     public double getRadius() {
         return radius;
     }
+
     public void setRadius(double radius) {
         this.radius = radius;
     }
